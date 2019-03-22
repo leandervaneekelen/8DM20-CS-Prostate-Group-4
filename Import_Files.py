f31708586af8443e4c478833c4de78d89773e416
ACvanDorp <48676678+ACvanDorp@users.noreply.github.com>
2019-03-22 11:07:36 +0100

    Update Import_Files.py

--- Import_Files.py
+++ Import_Files.py
@@ -8,7 +8,6 @@ import numpy as np
 def Import_Files(path):
     """Creates arrays consisting of the image data or image masks. Dimensions of the arrays are [15,86,333,271] consisiting of [patients,slices z direction, x , y] """
     folders = glob.glob(path+r'\p*') 
-    folders = glob.glob(folders) 
        
     masks = []
     MRI = []
@@ -27,9 +26,7 @@ def Import_Files(path):
 def Import_Files_string(path):
     """Creates arrays consisting of the directory's for all images"""
     folders = glob.glob(path+r'\p*') 
-    
-    patient = folders[0]
-    
+
     masks = []
     MRI = []
     for patient in folders:
@@ -49,6 +46,8 @@ path= r'C:\Users\s081992\Documents\TUE\Year 2\Q3\Capita Selecta\Part 2\TrainingD
 #fig, ax = plt.subplots(1, 2)
 #p = random.randint(0,14)
 #n = random.randint(22,50)
+#ax[0].imshow(MRI[p,n], cmap='gray')
+#ax[1].imshow(masks[p,n], cmap='gray')
 
 #%% Visualise Random patients Mid range prostate
 #p = random.randint(0,14)
