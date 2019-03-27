f4431a069dec2fcab58ebb817e34c7c35bc4621f
leandervaneekelen <leandervaneekelen@home.nl>
2019-03-27 15:15:15 +0100

    Made script into a function suitable for pipeline

--- Mutualinformation.py
+++ Mutualinformation.py
@@ -2,27 +2,27 @@
 """
 Created on Mon Mar 25 14:22:35 2019
 
-@author: s154994
+@author: s154994/s159890
 """
-if __name__ == '__main__':
-    user = 'Merel'
-    
-    if user == 'Merel':
-        path = r'C:\Users\s154994\Documents\8DM20_Capita_selecta_EHV\TrainingData'
 
-        
-import SimpleITK as sitk
-import matplotlib.pyplot as plt
 import numpy as np
 
-
-
-#mat_mi= #15x15 matrix met de mutual information over de gehele tabel
-mat_mi=np.arange(225).reshape(15, 15)
-
-atlasselection=list()
-for i in range(mat_mi.shape[0]):
-    coord_atlasel=np.nonzero(mat_mi[i,:]/mat_mi[i,:].max()>0.7)
-    atlasselection.append(coord_atlasel[0])
+def atlasfiltering(mat_mi, treshold = 0.7):
+    """Takes as input a 2 dimensional numpy array describing for all leave-n-out iterations what the
+    mutual information is of image i with all registered images. Diagonal is filled with NaNs (images
+    are not compared with themselves). Optional argument treshold describes treshold (alpha, see 
+    Klein et al.) that needs to be met for the atlas to pass the filtering.
     
+    Returns a list with, per row, a 1 dimensional numpy array describing what atlases met the treshold."""
     
+    atlasselection = list()
+    
+    for i in range(mat_mi.shape[0]):
+        # Normalise all MI's with the maximum out of that row, then treshold
+        tresholded_mat_mi = mat_mi[i,:]/mat_mi[i,:].max() > treshold
+        coord_atlases = np.nonzero(tresholded_mat_mi)
+        atlasselection.append(coord_atlases[0])
+    
+    return atlasselection
+
+
