d4325bd633e3816e27730a3db7862c1c6e37e428
Eekelen <l.v.eekelen@student.tue.nl>
2019-04-01 12:58:05 +0200

    Added Elastix to dev branch for easier use

--- pipeline.py
+++ pipeline.py
@@ -1,2 +1,28 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  1 11:10:26 2019
+
+@author: s159890, group 4 of 8DM20, Capita Selecta prostate
+"""
+
+# A new day, a fresh start
+
+#pipeline.py
+# Main script for 8DM20 Capita Selecta of Image Analysis, Prostate, group 4
+
+# Import all necessary methods written by group
+from newmethods import decisionfusing as df
+from newmethods import Mutualinformation as mi
+from Import_Files import Import_Files_string
+
+# Import all packages relevant for pipeline
+import numpy as np
+import SimpleITK as sitk
+
+# Define paths
+DATA_PATH = " "
+ELASTIX_PATH = " "
+RESULT_PATH = " "
+
+# Get data from 
 
